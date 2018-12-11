cd52cfece85122e7a4be5d01cf4ea780ada5aa2d
Tyler Ball <tyler@tylerball.net>
2018-12-11 15:05:57 -0500

    fix syntax error

--- pages/edit-products.js
+++ pages/edit-products.js
@@ -44,10 +44,9 @@ class EditProduct extends React.Component {
     const { name, price, discount, variantId, showToast } = this.state;
 
     return (
-      return (
-        <Mutation
-          mutation={UPDATE_PRICE}
-        >
+      <Mutation
+        mutation={UPDATE_PRICE}
+      >
         {(handleSubmit, {error, data}) => {
           const showError = error && (
             <Banner status="critical">{error.message}</Banner>
@@ -59,62 +58,62 @@ class EditProduct extends React.Component {
             />
           );
           return (
-          <Page>
-            <Layout>
-              {showToast}
-              {showError}
-              <Layout.Section>
-                <DisplayText size="large">{name}</DisplayText>
-                <Form>
-                  <Card sectioned>
-                    <FormLayout>
-                      <FormLayout.Group>
-                        <TextField
-                          prefix="$"
-                          value={price}
-                          disabled={true}
-                          label="Original price"
-                          type="price"
-                        />
-                        <TextField
-                          prefix="$"
-                          value={discount}
-                          onChange={this.handleChange('discount')}
-                          label="Discounted price"
-                          type="discount"
-                        />
-                      </FormLayout.Group>
-                      <p>
-                        This sale price will expire in two weeks on{' '}
-                        {this.props.expires}
-                      </p>
-                    </FormLayout>
-                  </Card>
-                  <PageActions
-                    primaryAction={[
-                      {
-                        content: 'Save',
-                        onAction: () => {
-                          const productVariableInput = {
-                            id: variantId,
-                            price: discount,
-                          };
-                          handleSubmit({
-                            variables: { input: productVariableInput }
-                          });
+            <Page>
+              <Layout>
+                {showToast}
+                {showError}
+                <Layout.Section>
+                  <DisplayText size="large">{name}</DisplayText>
+                  <Form>
+                    <Card sectioned>
+                      <FormLayout>
+                        <FormLayout.Group>
+                          <TextField
+                            prefix="$"
+                            value={price}
+                            disabled={true}
+                            label="Original price"
+                            type="price"
+                          />
+                          <TextField
+                            prefix="$"
+                            value={discount}
+                            onChange={this.handleChange('discount')}
+                            label="Discounted price"
+                            type="discount"
+                          />
+                        </FormLayout.Group>
+                        <p>
+                          This sale price will expire in two weeks on{' '}
+                          {this.props.expires}
+                        </p>
+                      </FormLayout>
+                    </Card>
+                    <PageActions
+                      primaryAction={[
+                        {
+                          content: 'Save',
+                          onAction: () => {
+                            const productVariableInput = {
+                              id: variantId,
+                              price: discount,
+                            };
+                            handleSubmit({
+                              variables: { input: productVariableInput }
+                            });
+                          },
                         },
-                      },
-                    ]}
-                    secondaryActions={[
-                      {
-                        content: 'Remove Discount',
-                      },
-                    ]}
-                  />
-                </Form>
-              </Layout.Section>
-            </Layout>
-          </Page>
+                      ]}
+                      secondaryActions={[
+                        {
+                          content: 'Remove Discount',
+                        },
+                      ]}
+                    />
+                  </Form>
+                </Layout.Section>
+              </Layout>
+            </Page>
           )
         }}
       </Mutation>
