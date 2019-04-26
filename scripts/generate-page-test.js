34532571aa21ca4d3e9fb20faaec84a22c7d7488
Katie Davis <katie.davis@shopify.com>
2019-04-26 15:04:45 -0400

    remove files

--- scripts/generate-page-test.js
+++
@@ -1,7 +0,0 @@
-const assert = require('assert');
-const generatePage = require('./generate-page.js');
-
-const output = generatePage('/tmp', ['npm', 'run-script', 'generate-page', 'katie']);
-const expectedOutput = console.log('/tmp/katie.js was successfully created!');
-
-assert(output === expectedOutput, 'Test case is true so this will not be printed');
