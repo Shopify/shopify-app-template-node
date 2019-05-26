5c2741adb62ab436467d5fdc2fee68fab8d20dc7
Katie Davis <katie.davis@shopify.com>
2019-05-26 13:08:00 -0400

    add utility to create component ready strings

--- scripts/tests/utilities.test.js
+++ scripts/tests/utilities.test.js
@@ -1,5 +1,12 @@
 const { createWebhooksUrl } = require("../utilities");
+const { toPascalCase } = require("../utilities");
 
 it("should transform string into webhooks specific url", () => {
   expect(createWebhooksUrl("TEST_CREATE")).toBe("/webhooks/test/create");
+});
+
+it("should transform string and remove characters to create PascalCase string", () => {
+  expect(toPascalCase("TEST_CREATE")).toBe("TestCreate");
+  expect(toPascalCase("test cREATE")).toBe("TestCreate");
+  expect(toPascalCase("test create!~")).toBe("TestCreate");
 });
