0e37b8a8b762803b73928281a8db81bc3c3d43ac
Katie Davis <katie.davis@shopify.com>
2019-05-27 17:19:51 -0400

    update to match cli commands (#23)
    * update to match cli commands

--- scripts/tests/generate-webhook.test.js
+++ scripts/tests/generate-webhook.test.js
@@ -1,30 +1,22 @@
-const generateWebhooks = require("../webhooks/generate-webhooks");
+const generateWebhook = require("../webhook/generate-webhook");
 const parser = require("@babel/parser").parse;
 const generate = require("@babel/generator").default;
 const {
   server,
   transformedWithMoreWebhooks,
-  transformedWithWebhooksandEnv,
-  scopes
+  transformedWithWebhooksandEnv
 } = require("./server-mocks");
 
 it("should return the new code with call to billing api", () => {
   const ast = parser(server, { sourceType: "module" });
-  const parsedAst = generateWebhooks(ast, "TEST_TYPE");
+  const parsedAst = generateWebhook(ast, "TEST_TYPE");
   const newCode = generate(parsedAst).code;
   expect(newCode).toBe(transformedWithWebhooksandEnv);
 });
 
 it("should only add the webhook subscription and registration if receiveWehook is already in file", () => {
   const ast = parser(transformedWithWebhooksandEnv, { sourceType: "module" });
-  const parsedAst = generateWebhooks(ast, "TEST_TWO");
+  const parsedAst = generateWebhook(ast, "TEST_TWO");
   const newCode = generate(parsedAst).code;
   expect(newCode).toBe(transformedWithMoreWebhooks);
-});
-
-it("should not add new scope if scope is already there", () => {
-  const ast = parser(scopes, { sourceType: "module" });
-  const parsedAst = generateWebhooks(ast, "TEST_TWO");
-  const newCode = generate(parsedAst).code;
-  expect(newCode).toBe(scopes);
 });
--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,7 +1,7 @@
-const createWebhooksUrl = type => {
+const createWebhookUrl = type => {
   const transformedString = type.replace("_", "/");
   return `/webhooks/${transformedString.toLowerCase()}`;
 };
 module.exports = {
-  createWebhooksUrl
+  createWebhookUrl
 };
