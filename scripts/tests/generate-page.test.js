354cc27c67f5c6ff799542b6564baf60853947c5
Katie Davis <katie.davis@shopify.com>
2019-05-29 16:06:44 -0400

    update as per PR feedback

--- scripts/tests/generate-page.test.js
+++ scripts/tests/generate-page.test.js
@@ -7,6 +7,7 @@ const Index = () => (
   <Page>
     <Heading>You created a new page called Index</Heading>
     <p>You can visit this page by appending \"/Index\" to your URL</p>
+    <p>You can edit this page in "/pages/index</p>
     <p>For information on Next.js dynamic routing <a href=\"https://nextjs.org/learn/basics/navigate-between-pages\" target=\"_blank\">check out their documentation</a></p>
     <p>For information about navigation within the admin frame, <a href=\"https://help.shopify.com/en/api/embedded-apps/app-extensions/navigation/create-navigation-link\" target=\"_blank\">see the Shopify documentation.</a></p>
   </Page>
