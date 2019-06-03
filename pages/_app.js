35291ac3dd461b6e56970e867c91c51f2fecd53f
Katie Davis <katie.davis@shopify.com>
2019-06-03 13:36:13 -0400

    Add proxy and apollo client (#30)

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 import App, { Container } from "next/app";
 import { AppProvider } from "@shopify/polaris";
 import { Provider } from "@shopify/app-bridge-react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 import "@shopify/polaris/styles.css";
 
+const client = new ApolloClient({
+  fetchOptions: {
+    credentials: "include"
+  }
+});
 class MyApp extends App {
   static async getInitialProps(server) {
     const shopOrigin = server.ctx.query.shop;
@@ -14,7 +21,9 @@ class MyApp extends App {
       <Container>
         <AppProvider>
           <Provider config={{ apiKey: API_KEY, shopOrigin: shopOrigin }}>
-            <Component {...pageProps} />
+            <ApolloProvider client={client}>
+              <Component {...pageProps} />
+            </ApolloProvider>
           </Provider>
         </AppProvider>
       </Container>
