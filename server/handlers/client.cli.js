e83f050f6fef7f02505c0583937f46b528c4770b
Dave Newton <dave.newton@shopify.com>
2019-06-18 14:09:11 -0400

    add separate client (and UA) for CLI

---
+++ server/handlers/client.cli.js
@@ -0,0 +1,17 @@
+import ApolloClient from "apollo-boost";
+
+export const createClient = (shop, accessToken) => {
+  return new ApolloClient({
+    uri: `https://${shop}/admin/api/2019-07/graphql.json`,
+    request: operation => {
+      operation.setContext({
+        headers: {
+          "X-Shopify-Access-Token": accessToken,
+          "User-Agent": `shopify-app-node ${
+            process.env.npm_package_version
+          } | Shopify App CLI`
+        }
+      });
+    }
+  });
+};
