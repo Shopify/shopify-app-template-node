44f96693d945e5dc40c917a3b887f38c63acb191
Katie Davis <katie.davis@shopify.com>
2019-09-13 12:21:37 -0400

    Remove scripts (#46)

--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ import next from "next";
 import Router from "koa-router";
 import session from "koa-session";
 import * as handlers from "./handlers/index";
-
 dotenv.config();
 const port = parseInt(process.env.PORT, 10) || 8081;
 const dev = process.env.NODE_ENV !== "production";
@@ -22,33 +21,35 @@ app.prepare().then(() => {
   const router = new Router();
   server.use(session(server));
   server.keys = [SHOPIFY_API_SECRET_KEY];
-
   server.use(
     createShopifyAuth({
       apiKey: SHOPIFY_API_KEY,
       secret: SHOPIFY_API_SECRET_KEY,
       scopes: [SCOPES],
+
       async afterAuth(ctx) {
         //Auth token and shop available in session
         //Redirect to shop upon auth
         const { shop, accessToken } = ctx.session;
-        ctx.cookies.set("shopOrigin", shop, { httpOnly: false });
+        ctx.cookies.set("shopOrigin", shop, {
+          httpOnly: false
+        });
         ctx.redirect("/");
       }
     })
   );
-
-  server.use(graphQLProxy({ version: ApiVersion.July19 }));
-
+  server.use(
+    graphQLProxy({
+      version: ApiVersion.July19
+    })
+  );
   router.get("*", verifyRequest(), async ctx => {
     await handle(ctx.req, ctx.res);
     ctx.respond = false;
     ctx.res.statusCode = 200;
   });
-
   server.use(router.allowedMethods());
   server.use(router.routes());
-
   server.listen(port, () => {
     console.log(`> Ready on http://localhost:${port}`);
   });
