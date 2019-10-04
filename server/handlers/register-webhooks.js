897b601fe1a107fd424fd816969df20d2678e3ef
Katie Davis <katie.davis@shopify.com>
2019-10-04 12:58:28 -0400

    adds version to webhook registration

--- server/handlers/register-webhooks.js
+++ server/handlers/register-webhooks.js
@@ -1,11 +1,18 @@
 import { registerWebhook } from "@shopify/koa-shopify-webhooks";
 
-export const registerWebhooks = async (shop, accessToken, type, url) => {
+export const registerWebhooks = async (
+  shop,
+  accessToken,
+  type,
+  url,
+  apiVersion
+) => {
   const registration = await registerWebhook({
     address: `${process.env.HOST}${url}`,
     topic: type,
     accessToken,
-    shop
+    shop,
+    apiVersion: apiVersion
   });
 
   if (registration.success) {
