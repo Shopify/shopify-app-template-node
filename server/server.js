347d5b934531e17cb1dda34f638083778464bea1
Paulo Margarido <paulo.margarido@shopify.com>
2021-03-09 14:51:32 -0500

    Tweaking package lock to next 10.0.4

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import Shopify, { ApiVersion } from "@shopify/shopify-api";
 import Koa from "koa";
 import next from "next";
 import Router from "koa-router";
+import "regenerator-runtime";
 
 dotenv.config();
 const port = parseInt(process.env.PORT, 10) || 8081;
