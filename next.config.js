a3c2372009afcff9efb2927bf3587e4fbb2f268a
Paulo Margarido <paulo.margarido@shopify.com>
2021-03-10 11:32:51 -0500

    Remove unnecessary withCSS call

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 const { parsed: localEnv } = require("dotenv").config();
-const withCSS = require("@zeit/next-css");
 
 const webpack = require("webpack");
 const apiKey = JSON.stringify(process.env.SHOPIFY_API_KEY);
 
-module.exports = withCSS({
+module.exports = {
   webpack: (config) => {
     const env = { API_KEY: apiKey };
     config.plugins.push(new webpack.DefinePlugin(env));
@@ -18,4 +17,4 @@ module.exports = withCSS({
 
     return config;
   },
-});
+};
