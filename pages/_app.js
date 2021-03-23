ecba1917524576655a22ddc07609e94e599d5095
Paulo Margarido <paulo.margarido@shopify.com>
2021-03-23 16:02:12 -0400

    Handle redirect header on proxy requests

--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,37 @@ import App from "next/app";
 import { AppProvider } from "@shopify/polaris";
 import { Provider, useAppBridge } from "@shopify/app-bridge-react";
 import { authenticatedFetch } from "@shopify/app-bridge-utils";
+import { Redirect } from "@shopify/app-bridge/actions";
 import "@shopify/polaris/dist/styles.css";
 import translations from "@shopify/polaris/locales/en.json";
+
+function userLoggedInFetch(app) {
+  const fetchFunction = authenticatedFetch(app);
+
+  return async (uri, options) => {
+    const response = await fetchFunction(uri, options);
+
+    if (
+      response.headers.get("X-Shopify-API-Request-Failure-Reauthorize") === "1"
+    ) {
+      const authUrlHeader = response.headers.get(
+        "X-Shopify-API-Request-Failure-Reauthorize-Url"
+      );
+
+      const redirect = Redirect.create(app);
+      redirect.dispatch(Redirect.Action.APP, authUrlHeader || `/auth`);
+      return null;
+    }
+
+    return response;
+  };
+}
 
 function MyProvider(props) {
   const app = useAppBridge();
 
   const client = new ApolloClient({
-    fetch: authenticatedFetch(app),
+    fetch: userLoggedInFetch(app),
     fetchOptions: {
       credentials: "include",
     },
