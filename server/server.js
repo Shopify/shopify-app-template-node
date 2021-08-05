d75576749d4ccc83e4ca189d00fa8839d636b734
NoharaMasato <masato338914914@gmail.com>
2021-08-05 13:38:34 +0900

    Remove trailing whitespace from HOST env variable.(#644)

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ Shopify.Context.initialize({
   API_KEY: process.env.SHOPIFY_API_KEY,
   API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
   SCOPES: process.env.SCOPES.split(","),
-  HOST_NAME: process.env.HOST.replace(/https:\/\//, ""),
+  HOST_NAME: process.env.HOST.replace(/https:\/\/|\/$/g, ""),
   API_VERSION: ApiVersion.October20,
   IS_EMBEDDED_APP: true,
   // This should be replaced with your preferred storage strategy
