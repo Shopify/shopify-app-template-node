53dfbfda7e867c8ac53182b78099c46b8a50b110
Melanie Wang <msmangochan@gmail.com>
2021-11-19 15:00:37 -0500

    Revert "Sample app updates"

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import { Heading, Page } from "@shopify/polaris";
+
+const Index = () => (
+  <Page>
+    <Heading>Shopify app with Node and React 🎉</Heading>
+  </Page>
+);
+
+export default Index;
---
+++ server/handlers/index.js
@@ -0,0 +1,5 @@
+import { createClient } from "./client";
+import { getOneTimeUrl } from "./mutations/get-one-time-url";
+import { getSubscriptionUrl } from "./mutations/get-subscription-url";
+
+export { createClient, getOneTimeUrl, getSubscriptionUrl };
