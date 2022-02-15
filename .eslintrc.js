174614cc926ec976682abbf8ae6ece3331248525
Jason Kurian <jason.kurian@shopify.com>
2022-02-15 13:48:34 -0500

    further simplify linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,3 @@
 module.exports = {
-  extends: [
-    "plugin:@shopify/react",
-    "plugin:@shopify/polaris",
-    "plugin:@shopify/jest",
-    "plugin:@shopify/webpack",
-    "next",
-  ],
-  rules: {
-    "import/no-unresolved": "off",
-  },
-  overrides: [
-    {
-      files: ["*.test.*"],
-      rules: {
-        "shopify/jsx-no-hardcoded-content": "off",
-      },
-    },
-  ],
+  extends: "next/core-web-vitals",
 };
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,14 @@
-/* eslint-disable @shopify/jsx-no-hardcoded-content */
 import { Heading, Page } from "@shopify/polaris";
 
-const Index = () => (
-  <Page>
-    <Heading>
-      Shopify app with Node and React{" "}
-      <span role="img" aria-label="tada emoji">
-        🎉
-      </span>
-    </Heading>
-  </Page>
-);
-
-export default Index;
+export default function Index() {
+  return (
+    <Page>
+      <Heading>
+        Shopify app with Node and React{" "}
+        <span role="img" aria-label="tada emoji">
+          🎉
+        </span>
+      </Heading>
+    </Page>
+  );
+}
