d131d06479c04da00132ac5174e8d525e906b802
Jason Kurian <jason.kurian@shopify.com>
2022-02-25 16:50:01 -0500

    add test todos
    Co-Authored-By: Paulo Margarido <64600052+paulomarg@users.noreply.github.com>

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,4 +9,12 @@ describe('vitest', async () => {
   test('works!', () => {
     request(app).get('/').expect(200);
   });
+
+  test.todo('make sure we go to oauth');
+  test.todo('when we go to oauth we get the toplevel');
+  test.todo('get proper redirection url to start oauth');
+  test.todo('handle the callback correctly');
+  test.todo('webhook processing');
+  test.todo('graphql proxy');
+  test.todo('a div with the app renders');
 });
