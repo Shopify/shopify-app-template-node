dada6e1bb2f81217ad92fac556d105c6cf0be868
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2022-03-29 09:38:27 -0400

    Updating to v3 RC3

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ Shopify.Context.initialize({
   API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
   SCOPES: process.env.SCOPES.split(","),
   HOST_NAME: process.env.HOST.replace(/https:\/\//, ""),
-  API_VERSION: ApiVersion.January22,
+  API_VERSION: ApiVersion.April22,
   IS_EMBEDDED_APP: true,
   // This should be replaced with your preferred storage strategy
   SESSION_STORAGE: new Shopify.Session.MemorySessionStorage(),
