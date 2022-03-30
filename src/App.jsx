0ebd88a163bc89bfc2188b383faed5d8d005a4a8
Joey Freund <joey.freund@shopify.com>
2022-03-30 18:56:46 +0300

    simplify app-bridge initialization code

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,6 @@ export default function App() {
         config={{
           apiKey: process.env.SHOPIFY_API_KEY,
           host: new URL(location).searchParams.get("host"),
-          forceRedirect: true,
         }}
       >
         <MyProvider>
