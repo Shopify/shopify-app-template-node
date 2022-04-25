87743e9653866ca25c15db1eda7d19ee85ce1575
Surma <surma@shopify.com>
2022-04-25 13:05:02 +0100

    Add GDPR webhooks

---
+++ home/backend/gdpr.js
@@ -0,0 +1,80 @@
+import { Shopify } from "@shopify/shopify-api";
+
+export function setupGDPRWebHooks() {
+  /**
+   * Customers can request their data from a store owner. When this happens,
+   * Shopify invokes this webhook.
+   *
+   * https://shopify.dev/apps/webhooks/configuration/mandatory-webhooks#customers-data_request
+   */
+  Shopify.Webhooks.Registry.addHandler("customers/data_request", {
+    path: "/webhooks",
+    webhookHandler: async (topic, shop, body) => {
+      const payload = JSON.parse(body);
+      // Payload has the following shape:
+      // {
+      //	"shop_id": 954889,
+      //	"shop_domain": "{shop}.myshopify.com",
+      //	"orders_requested": [
+      //		299938,
+      //		280263,
+      //		220458
+      //	],
+      //	"customer": {
+      //		"id": 191167,
+      //		"email": "john@example.com",
+      //		"phone": "555-625-1199"
+      //	},
+      //	"data_request": {
+      //		"id": 9999
+      //	}
+      //}
+    },
+  });
+
+  /**
+   * Store owners can request that data is deleted on behalf of a customer. When
+   * this happens, Shopify invokes this webhook.
+   *
+   * https://shopify.dev/apps/webhooks/configuration/mandatory-webhooks#customers-redact
+   */
+  Shopify.Webhooks.Registry.addHandler("customers/redact", {
+    path: "/webhooks",
+    webhookHandler: async (topic, shop, body) => {
+      const payload = JSON.parse(body);
+      // Payload has the following shape:
+      // {
+      // 	"shop_id": 954889,
+      // 	"shop_domain": "{shop}.myshopify.com",
+      // 	"customer": {
+      // 		"id": 191167,
+      // 		"email": "john@example.com",
+      // 		"phone": "555-625-1199"
+      // 	},
+      // 	"orders_to_redact": [
+      // 		299938,
+      // 		280263,
+      // 		220458
+      // 	]
+      // }
+    },
+  });
+
+  /**
+   * 48 hours after a store owner uninstalls your app, Shopify invokes this
+   * webhook.
+   *
+   * https://shopify.dev/apps/webhooks/configuration/mandatory-webhooks#shop-redact
+   */
+  Shopify.Webhooks.Registry.addHandler("shop/redact", {
+    path: "/webhooks",
+    webhookHandler: async (topic, shop, body) => {
+      const payload = JSON.parse(body);
+      // Payload has the following shape:
+      // {
+      // 	"shop_id": 954889,
+      // 	"shop_domain": "{shop}.myshopify.com"
+      // }
+    },
+  });
+}
