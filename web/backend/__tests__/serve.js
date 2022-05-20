c9d37971a93fa9d185bce1ded304bd8cdb483473
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2022-05-20 16:36:53 -0400

    Minor adjustments to the backend app
     - Add SQLite database file to .gitignore
     - Update default CLI version
     - Replace .env.test with hard-coded values in the test setup so we can
       run backend tests
     - Remove build command from the BE, it now happens solely in the FE,
       and the BE simply points to the right file

--- web/backend/__tests__/serve.js
+++ web/backend/__tests__/serve.js
@@ -1,6 +1,13 @@
 import path from "path";
 
-const port = 9528;
+// Hard-code some dummy values for the environment variables so that we can run the tests without needing to set them up
+// If the CLI ever evolves to run the tests itself, we can rely on it to give us these values
+process.env.SHOPIFY_API_KEY = "TEST_SHOPIFY_API_KEY";
+process.env.SHOPIFY_API_SECRET = "TEST_SHOPIFY_SECRET";
+process.env.HOST = "TEST_TUNNEL_URL";
+process.env.SCOPES = "TEST_SCOPES";
+process.env.BACKEND_PORT = 9528;
+process.env.FRONTEND_PORT = 9529;
 
 /**
  * @param {string} root
@@ -25,6 +32,6 @@ export async function serve(root, isProd) {
   const { createServer } = await import(
     path.resolve(root, "backend", "index.js")
   );
-  process.env.PORT = port;
+
   return await createServer(root, isProd);
 }
