587c28f049ae1b12f57f78404129edf4e149b60e
Kevin O'Sullivan <mkevin.osullivan@shopify.com>
2022-07-20 20:14:28 -0400

    Add await to SessionStorage methods calls

--- web/app_installations.js
+++ web/app_installations.js
@@ -2,10 +2,10 @@ import { Shopify } from "@shopify/shopify-api";
 
 export const AppInstallations = {
   includes: async function (shopDomain) {
-    const shopSessions = Shopify.Context.SESSION_STORAGE.findSessionsByShop(shopDomain);
+    const shopSessions = await Shopify.Context.SESSION_STORAGE.findSessionsByShop(shopDomain);
 
     if (shopSessions.length > 0) {
-      for (const session in shopSessions) {
+      for (const session of shopSessions) {
         if (session.accessToken) return true;
       }
     }
@@ -14,9 +14,9 @@ export const AppInstallations = {
   },
 
   delete: async function (shopDomain) {
-    const shopSessions = Shopify.Context.SESSION_STORAGE.findSessionsByShop(shopDomain);
+    const shopSessions = await Shopify.Context.SESSION_STORAGE.findSessionsByShop(shopDomain);
     if (shopSessions.length > 0) {
-      Shopify.Context.SESSION_STORAGE.deleteSessions(shopSessions.map((session) => session.id));
+      await Shopify.Context.SESSION_STORAGE.deleteSessions(shopSessions.map((session) => session.id));
     }
   },
 };
