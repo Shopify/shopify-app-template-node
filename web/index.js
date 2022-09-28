257892016273e31f4142161d11245ab8e21fd49f
Kevin O'Sullivan <mkevin.osullivan@shopify.com>
2022-09-27 23:48:58 -0400

    Refactor CLI v3 template to use v6 of Node API lib

--- web/index.js
+++ web/index.js
@@ -3,14 +3,19 @@ import { join } from "path";
 import { readFileSync } from "fs";
 import express from "express";
 import cookieParser from "cookie-parser";
-import { Shopify, LATEST_API_VERSION } from "@shopify/shopify-api";
+import "@shopify/shopify-api/adapters/node";
+import {
+  shopifyApi,
+  BillingInterval,
+  LATEST_API_VERSION,
+} from "@shopify/shopify-api";
+import { SQLiteSessionStorage } from "@shopify/shopify-api/session-storage/sqlite";
 
 import applyAuthMiddleware from "./middleware/auth.js";
 import verifyRequest from "./middleware/verify-request.js";
 import { setupGDPRWebHooks } from "./gdpr.js";
 import productCreator from "./helpers/product-creator.js";
 import redirectToAuth from "./helpers/redirect-to-auth.js";
-import { BillingInterval } from "./helpers/ensure-billing.js";
 import { AppInstallations } from "./app_installations.js";
 
 const USE_ONLINE_TOKENS = false;
@@ -23,43 +28,39 @@ const PROD_INDEX_PATH = `${process.cwd()}/frontend/dist/`;
 
 const DB_PATH = `${process.cwd()}/database.sqlite`;
 
-Shopify.Context.initialize({
-  API_KEY: process.env.SHOPIFY_API_KEY,
-  API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
-  SCOPES: process.env.SCOPES.split(","),
-  HOST_NAME: process.env.HOST.replace(/https?:\/\//, ""),
-  HOST_SCHEME: process.env.HOST.split("://")[0],
-  API_VERSION: LATEST_API_VERSION,
-  IS_EMBEDDED_APP: true,
-  // This should be replaced with your preferred storage strategy
-  // See note below regarding using CustomSessionStorage with this template.
-  SESSION_STORAGE: new Shopify.Session.SQLiteSessionStorage(DB_PATH),
-  ...(process.env.SHOP_CUSTOM_DOMAIN && {CUSTOM_SHOP_DOMAINS: [process.env.SHOP_CUSTOM_DOMAIN]}),
-});
-
-// NOTE: If you choose to implement your own storage strategy using
-// Shopify.Session.CustomSessionStorage, you MUST implement the optional
-// findSessionsByShopCallback and deleteSessionsCallback methods.  These are
-// required for the app_installations.js component in this template to
-// work properly.
-
-Shopify.Webhooks.Registry.addHandler("APP_UNINSTALLED", {
-  path: "/api/webhooks",
-  webhookHandler: async (_topic, shop, _body) => {
-    await AppInstallations.delete(shop);
-  },
-});
-
 // The transactions with Shopify will always be marked as test transactions, unless NODE_ENV is production.
 // See the ensureBilling helper to learn more about billing in this template.
-const BILLING_SETTINGS = {
-  required: false,
-  // This is an example configuration that would do a one-time charge for $5 (only USD is currently supported)
-  // chargeName: "My Shopify One-Time Charge",
-  // amount: 5.0,
-  // currencyCode: "USD",
-  // interval: BillingInterval.OneTime,
+const billingConfig = {
+  "My Shopify One-Time Charge": {
+    // This is an example configuration that would do a one-time charge for $5 (only USD is currently supported)
+    amount: 5.0,
+    currencyCode: "USD",
+    interval: BillingInterval.OneTime,
+  },
 };
+
+const appConfig = {
+  apiKey: process.env.SHOPIFY_API_KEY,
+  apiSecretKey: process.env.SHOPIFY_API_SECRET,
+  scopes: process.env.SCOPES.split(","),
+  hostName: process.env.HOST.replace(/https?:\/\//, ""),
+  hostScheme: process.env.HOST.split("://")[0],
+  apiVersion: LATEST_API_VERSION,
+  isEmbeddedApp: true,
+  // This should be replaced with your preferred storage strategy
+  sessionStorage: new SQLiteSessionStorage(DB_PATH),
+  billing: undefined, // or replace with billingConfig above to enable example billing
+};
+
+const shopify = shopifyApi(appConfig);
+
+shopify.webhooks.addHandler({
+  topic: "APP_UNINSTALLED",
+  path: "/api/webhooks",
+  webhookHandler: async (_topic, shop, _body) => {
+    await AppInstallations.delete(shop, shopify);
+  },
+});
 
 // This sets up the mandatory GDPR webhooks. You’ll need to fill in the endpoint
 // in the “GDPR mandatory webhooks” section in the “App setup” tab, and customize
@@ -67,30 +68,31 @@ const BILLING_SETTINGS = {
 //
 // More details can be found on shopify.dev:
 // https://shopify.dev/apps/webhooks/configuration/mandatory-webhooks
-setupGDPRWebHooks("/api/webhooks");
+setupGDPRWebHooks(shopify, "/api/webhooks");
 
 // export for test use only
 export async function createServer(
   root = process.cwd(),
-  isProd = process.env.NODE_ENV === "production",
-  billingSettings = BILLING_SETTINGS
+  isProd = process.env.NODE_ENV === "production"
 ) {
   const app = express();
 
   app.set("use-online-tokens", USE_ONLINE_TOKENS);
-  app.use(cookieParser(Shopify.Context.API_SECRET_KEY));
+  app.use(cookieParser(shopify.config.apiSecretKey));
 
-  applyAuthMiddleware(app, {
-    billing: billingSettings,
-  });
+  applyAuthMiddleware(shopify, app);
 
-  // Do not call app.use(express.json()) before processing webhooks with
-  // Shopify.Webhooks.Registry.process().
-  // See https://github.com/Shopify/shopify-api-node/blob/main/docs/usage/webhooks.md#note-regarding-use-of-body-parsers
-  // for more details.
+  // All endpoints after this point will have access to a request.body
+  // attribute, as a result of the express.json() middleware
+  app.use(express.json());
+
   app.post("/api/webhooks", async (req, res) => {
     try {
-      await Shopify.Webhooks.Registry.process(req, res);
+      await shopify.webhooks.process({
+        rawBody: JSON.stringify(req.body),
+        rawRequest: req,
+        rawResponse: res,
+      });
       console.log(`Webhook processed, returned status code 200`);
     } catch (e) {
       console.log(`Failed to process webhook: ${e.message}`);
@@ -101,21 +103,16 @@ export async function createServer(
   });
 
   // All endpoints after this point will require an active session
-  app.use(
-    "/api/*",
-    verifyRequest(app, {
-      billing: billingSettings,
-    })
-  );
+  app.use("/api/*", verifyRequest(shopify, app));
 
   app.get("/api/products/count", async (req, res) => {
-    const session = await Shopify.Utils.loadCurrentSession(
-      req,
-      res,
-      app.get("use-online-tokens")
-    );
+    const session = await shopify.session.getCurrent({
+      isOnline: app.get("use-online-tokens"),
+      rawRequest: req,
+      rawResponse: res,
+    });
     const { Product } = await import(
-      `@shopify/shopify-api/dist/rest-resources/${Shopify.Context.API_VERSION}/index.js`
+      `@shopify/shopify-api/rest/admin/${shopify.config.apiVersion}`
     );
 
     const countData = await Product.count({ session });
@@ -123,16 +120,16 @@ export async function createServer(
   });
 
   app.get("/api/products/create", async (req, res) => {
-    const session = await Shopify.Utils.loadCurrentSession(
-      req,
-      res,
-      app.get("use-online-tokens")
-    );
+    const session = await shopify.session.getCurrent({
+      isOnline: app.get("use-online-tokens"),
+      rawRequest: req,
+      rawResponse: res,
+    });
     let status = 200;
     let error = null;
 
     try {
-      await productCreator(session);
+      await productCreator(session, shopify);
     } catch (e) {
       console.log(`Failed to process products/create: ${e.message}`);
       status = 500;
@@ -141,13 +138,9 @@ export async function createServer(
     res.status(status).send({ success: status === 200, error });
   });
 
-  // All endpoints after this point will have access to a request.body
-  // attribute, as a result of the express.json() middleware
-  app.use(express.json());
-
   app.use((req, res, next) => {
-    const shop = Shopify.Utils.sanitizeShop(req.query.shop);
-    if (Shopify.Context.IS_EMBEDDED_APP && shop) {
+    const shop = shopify.utils.sanitizeShop(req.query.shop);
+    if (shopify.config.isEmbeddedApp && shop) {
       res.setHeader(
         "Content-Security-Policy",
         `frame-ancestors https://${encodeURIComponent(
@@ -177,15 +170,18 @@ export async function createServer(
       return res.send("No shop provided");
     }
 
-    const shop = Shopify.Utils.sanitizeShop(req.query.shop);
-    const appInstalled = await AppInstallations.includes(shop);
+    const shop = shopify.utils.sanitizeShop(req.query.shop);
+    const appInstalled = await AppInstallations.includes(shop, shopify);
 
     if (!appInstalled && !req.originalUrl.match(/^\/exitiframe/i)) {
-      return redirectToAuth(req, res, app);
+      return redirectToAuth(req, res, shopify, app);
     }
 
-    if (Shopify.Context.IS_EMBEDDED_APP && req.query.embedded !== "1") {
-      const embeddedUrl = Shopify.Utils.getEmbeddedAppUrl(req);
+    if (shopify.config.isEmbeddedApp && req.query.embedded !== "1") {
+      const embeddedUrl = await shopify.auth.getEmbeddedAppUrl({
+        rawRequest: req,
+        rawResponse: res,
+      });
 
       return res.redirect(embeddedUrl + req.path);
     }
--- web/middleware/verify-request.js
+++ web/middleware/verify-request.js
@@ -1,7 +1,5 @@
-import { Shopify } from "@shopify/shopify-api";
-import ensureBilling, {
-  ShopifyBillingError,
-} from "../helpers/ensure-billing.js";
+import { BillingError, HttpResponseError } from "@shopify/shopify-api";
+import ensureBilling from "../helpers/ensure-billing.js";
 import redirectToAuth from "../helpers/redirect-to-auth.js";
 
 import returnTopLevelRedirection from "../helpers/return-top-level-redirection.js";
@@ -13,52 +11,42 @@ const TEST_GRAPHQL_QUERY = `
   }
 }`;
 
-export default function verifyRequest(
-  app,
-  { billing = { required: false } } = { billing: { required: false } }
-) {
+export default function verifyRequest(shopify, app) {
   return async (req, res, next) => {
-    const session = await Shopify.Utils.loadCurrentSession(
-      req,
-      res,
-      app.get("use-online-tokens")
-    );
+    const session = await shopify.session.getCurrent({
+      isOnline: app.get("use-online-tokens"),
+      rawRequest: req,
+      rawResponse: res,
+    });
 
-    let shop = Shopify.Utils.sanitizeShop(req.query.shop);
+    let shop = shopify.utils.sanitizeShop(req.query.shop);
     if (session && shop && session.shop !== shop) {
       // The current request is for a different shop. Redirect gracefully.
-      return redirectToAuth(req, res, app);
+      return redirectToAuth(req, res, shopify, app);
     }
 
     if (session?.isActive()) {
       try {
-        if (billing.required) {
-          // The request to check billing status serves to validate that the access token is still valid.
-          const [hasPayment, confirmationUrl] = await ensureBilling(
-            session,
-            billing
-          );
-
-          if (!hasPayment) {
-            returnTopLevelRedirection(req, res, confirmationUrl);
-            return;
-          }
+        const [hasPayment, confirmationUrl] = await ensureBilling(session, shopify);
+        if (!hasPayment) {
+          returnTopLevelRedirection(req, res, confirmationUrl);
+          return;
         } else {
           // Make a request to ensure the access token is still valid. Otherwise, re-authenticate the user.
-          const client = new Shopify.Clients.Graphql(
-            session.shop,
-            session.accessToken
-          );
+          const client = new shopify.clients.Graphql({
+            domain: session.shop,
+            accessToken: session.accessToken
+          });
           await client.query({ data: TEST_GRAPHQL_QUERY });
         }
         return next();
       } catch (e) {
         if (
-          e instanceof Shopify.Errors.HttpResponseError &&
+          e instanceof HttpResponseError &&
           e.response.code === 401
         ) {
           // Re-authenticate if we get a 401 response
-        } else if (e instanceof ShopifyBillingError) {
+        } else if (e instanceof BillingError) {
           console.error(e.message, e.errorData[0]);
           res.status(500).end();
           return;
@@ -73,9 +61,9 @@ export default function verifyRequest(
       if (!shop) {
         if (session) {
           shop = session.shop;
-        } else if (Shopify.Context.IS_EMBEDDED_APP) {
+        } else if (shopify.config.isEmbeddedApp) {
           if (bearerPresent) {
-            const payload = Shopify.Utils.decodeSessionToken(bearerPresent[1]);
+            const payload = await shopify.session.decodeSessionToken(bearerPresent[1]);
             shop = payload.dest.replace("https://", "");
           }
         }
