c543d59bfde070145918d87d3771c2769be3b08d
Kevin O'Sullivan <mkevin.osullivan@shopify.com>
2022-10-11 16:22:38 -0400

    Add note re using CustomSessionStorage with template

--- web/index.js
+++ web/index.js
@@ -32,9 +32,16 @@ Shopify.Context.initialize({
   API_VERSION: LATEST_API_VERSION,
   IS_EMBEDDED_APP: true,
   // This should be replaced with your preferred storage strategy
+  // See note below regarding using CustomSessionStorage with this template.
   SESSION_STORAGE: new Shopify.Session.SQLiteSessionStorage(DB_PATH),
   ...(process.env.SHOP_CUSTOM_DOMAIN && {CUSTOM_SHOP_DOMAINS: [process.env.SHOP_CUSTOM_DOMAIN]}),
 });
+
+// NOTE: If you choose to implement your own storage strategy using
+// Shopify.Session.CustomSessionStorage, you MUST implement the optional
+// findSessionsByShopCallback and deleteSessionsCallback methods.  These are
+// required for the app_installations.js component in this template to
+// work properly.
 
 Shopify.Webhooks.Registry.addHandler("APP_UNINSTALLED", {
   path: "/api/webhooks",
