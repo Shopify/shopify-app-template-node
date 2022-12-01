32be79ab1c9c9d671218e4c2620f0ba0a283247f
Kevin O'Sullivan <mkevin.osullivan@shopify.com>
2022-12-01 11:39:05 -0500

    Use dynamic import for restResources, be explicit in apiVersion

--- web/shopify.js
+++ web/shopify.js
@@ -1,12 +1,15 @@
-import { LogSeverity } from "@shopify/shopify-api";
-import { restResources } from "@shopify/shopify-api/rest/admin/2022-10";
+import { LATEST_API_VERSION } from "@shopify/shopify-api";
 import { shopifyApp } from "@shopify/shopify-app-express";
 import { SQLiteSessionStorage } from "@shopify/shopify-app-session-storage-sqlite";
+let { restResources } = await import(`@shopify/shopify-api/rest/admin/${LATEST_API_VERSION}`);
+// If you want IntelliSense for the rest resources, you should import them directly
+// import { restResources } from "@shopify/shopify-api/rest/admin/2022-10";
 
 const DB_PATH = `${process.cwd()}/database.sqlite`;
 
 const shopify = shopifyApp({
   api: {
+    apiVersion: LATEST_API_VERSION,
     restResources,
   },
   // This should be replaced with your preferred storage strategy
