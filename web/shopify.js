238ef038b2219b2a046bf9f0cfad2e59ac93adb1
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2023-01-06 12:21:49 -0500

    Update shopify-app-express

--- web/shopify.js
+++ web/shopify.js
@@ -1,11 +1,7 @@
 import { BillingInterval, LATEST_API_VERSION } from "@shopify/shopify-api";
 import { shopifyApp } from "@shopify/shopify-app-express";
 import { SQLiteSessionStorage } from "@shopify/shopify-app-session-storage-sqlite";
-let { restResources } = await import(
-  `@shopify/shopify-api/rest/admin/${LATEST_API_VERSION}`
-);
-// If you want IntelliSense for the rest resources, you should import them directly
-// import { restResources } from "@shopify/shopify-api/rest/admin/2022-10";
+import { restResources } from "@shopify/shopify-api/rest/admin/2023-01";
 
 const DB_PATH = `${process.cwd()}/database.sqlite`;
 
