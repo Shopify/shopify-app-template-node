4330e36c3015422c6193129c20ab3cedd4e80cba
David Witt <david.witt@shopify.com>
2023-03-15 13:43:03 -0700

    Removed Product Creator code

--- web/index.js
+++ web/index.js
@@ -5,7 +5,6 @@ import express from "express";
 import serveStatic from "serve-static";
 
 import shopify from "./shopify.js";
-import productCreator from "./product-creator.js";
 import GDPRWebhookHandlers from "./gdpr.js";
 
 import { getTags, addTags, deleteTags } from "./api/product-tags.js";
@@ -48,20 +47,6 @@ app.get("/api/products/count", async (_req, res) => {
     session: res.locals.shopify.session,
   });
   res.status(200).send(countData);
-});
-
-app.get("/api/products/create", async (_req, res) => {
-  let status = 200;
-  let error = null;
-
-  try {
-    await productCreator(res.locals.shopify.session);
-  } catch (e) {
-    console.log(`Failed to process products/create: ${e.message}`);
-    status = 500;
-    error = e.message;
-  }
-  res.status(status).send({ success: status === 200, error });
 });
 
 app.use(serveStatic(STATIC_PATH, { index: false }));
