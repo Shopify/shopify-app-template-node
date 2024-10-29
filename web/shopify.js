4de25c671ca7aa3fce47d125a2db3e57daba80b3
Andy Liu <andy.liu@shopify.com>
2024-10-29 11:21:16 -0400

    rest resource version was missed

--- web/shopify.js
+++ web/shopify.js
@@ -1,7 +1,7 @@
 import { BillingInterval, LATEST_API_VERSION } from "@shopify/shopify-api";
 import { shopifyApp } from "@shopify/shopify-app-express";
 import { SQLiteSessionStorage } from "@shopify/shopify-app-session-storage-sqlite";
-import { restResources } from "@shopify/shopify-api/rest/admin/2024-07";
+import { restResources } from "@shopify/shopify-api/rest/admin/2024-10";
 
 const DB_PATH = `${process.cwd()}/database.sqlite`;
 
